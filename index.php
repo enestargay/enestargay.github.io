0fecdd1decfe50ad80c83eb4176c126f287dff31
Yusuf Enes TARGAY <71759420+enestargay@users.noreply.github.com>
2021-04-06 12:00:14 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'tema/header.php'; ?>
-<?php include 'tema/silider.php'; ?>
-<?php include 'tema/son-eklenenler.php'; ?>
-<?php include 'tema/postlar.php'; ?>
-<?php include 'tema/populer-yazilar.php'; ?>
-<?php include 'tema/footer.php'; ?>
